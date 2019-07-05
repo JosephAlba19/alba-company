2d1800f015642e60c998d644337501ee486e075f
Joseph Alba <jalba19@gmail.com>
2019-07-04 22:01:39 -0400

    Reverted navbar from bootsrap.css to vanilla scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('js', function () {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+    return gulp.src(['dist/js/app.js'])
         .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
 });
@@ -21,7 +21,7 @@ gulp.task('serve', ['sass'], function () {
         server: "./src"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['src/scss/*.scss'], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
